ad224b47d31068c3ed90456d43b936de3827ce8d
Christo Joseph <1538412+josephch@users.noreply.github.com>
2025-07-05 14:46:10 +0530

    Use space instead of tabs in formatted print

--- cJSON.c
+++ cJSON.c
@@ -1779,13 +1779,22 @@ static cJSON_bool print_object(const cJSON * const item, printbuffer * const out
     }
 
     /* Compose the output: */
-    length = (size_t) (output_buffer->format ? 2 : 1); /* fmt: {\n */
+    length = (size_t) (output_buffer->format ? 3 + output_buffer->depth*2 : 1); /* fmt: \n...{\n */
     output_pointer = ensure(output_buffer, length + 1);
     if (output_pointer == NULL)
     {
         return false;
     }
 
+    if (output_buffer->format)
+    {
+        *output_pointer++ = '\n';
+        for (int i = 0; i < output_buffer->depth; i++)
+        {
+            *output_pointer++ = ' ';
+            *output_pointer++ = ' ';
+        }
+    }
     *output_pointer++ = '{';
     output_buffer->depth++;
     if (output_buffer->format)
@@ -1799,16 +1808,16 @@ static cJSON_bool print_object(const cJSON * const item, printbuffer * const out
         if (output_buffer->format)
         {
             size_t i;
-            output_pointer = ensure(output_buffer, output_buffer->depth);
+            output_pointer = ensure(output_buffer, output_buffer->depth*2);
             if (output_pointer == NULL)
             {
                 return false;
             }
-            for (i = 0; i < output_buffer->depth; i++)
+            for (i = 0; i < output_buffer->depth*2; i++)
             {
-                *output_pointer++ = '\t';
+                *output_pointer++ = ' ';
             }
-            output_buffer->offset += output_buffer->depth;
+            output_buffer->offset += output_buffer->depth * 2;
         }
 
         /* print key */
@@ -1818,16 +1827,20 @@ static cJSON_bool print_object(const cJSON * const item, printbuffer * const out
         }
         update_offset(output_buffer);
 
-        length = (size_t) (output_buffer->format ? 2 : 1);
+        length = (size_t) (output_buffer->format ? 3 : 1);
         output_pointer = ensure(output_buffer, length);
         if (output_pointer == NULL)
         {
             return false;
         }
+        if (output_buffer->format)
+        {
+            *output_pointer++ = ' ';
+        }
         *output_pointer++ = ':';
         if (output_buffer->format)
         {
-            *output_pointer++ = '\t';
+            *output_pointer++ = ' ';
         }
         output_buffer->offset += length;
 
@@ -1860,7 +1873,7 @@ static cJSON_bool print_object(const cJSON * const item, printbuffer * const out
         current_item = current_item->next;
     }
 
-    output_pointer = ensure(output_buffer, output_buffer->format ? (output_buffer->depth + 1) : 2);
+    output_pointer = ensure(output_buffer, output_buffer->format ? (output_buffer->depth*2 + 1) : 2);
     if (output_pointer == NULL)
     {
         return false;
@@ -1868,9 +1881,9 @@ static cJSON_bool print_object(const cJSON * const item, printbuffer * const out
     if (output_buffer->format)
     {
         size_t i;
-        for (i = 0; i < (output_buffer->depth - 1); i++)
+        for (i = 0; i < (output_buffer->depth - 1) *2; i++)
         {
-            *output_pointer++ = '\t';
+            *output_pointer++ = ' ';
         }
     }
     *output_pointer++ = '}';
